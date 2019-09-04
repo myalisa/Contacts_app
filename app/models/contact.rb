b9ef77676b16789f737c2faf33f7839141f22da7
myalisa <myalisamiro@gmail.com>
2019-09-04 11:33:01 -0500

    Updated Partials

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%A, %b %d")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japan_number
+    
+    
+  end
 end
+
+
